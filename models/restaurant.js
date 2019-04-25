df90ab20bfaddafe77109904af869367dd6caae6
miayang <miayang0513@yangminande-MacBook-Pro.local>
2019-04-26 02:27:07 +0800

    Add model of restaurant  and seed of user

---
+++ models/restaurant.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const restaurantSchema = new Schema({
+	name: String,
+  name_en: String,
+  category: String,
+  image: String,
+  location: String,
+  phone: String,
+  google_map: String,
+  rating: Number,
+  description: String,
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
